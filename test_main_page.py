3e18d575a931277220ca08e16eba1db374ef5559
susannabdoyan <bdoyansus@gmail.com>
2022-09-27 19:19:20 +0400

    file added

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 #import pytest
 #from selenium import webdriver
 #from selenium.webdriver.common.by import By
+
 from .page_object.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,7 +9,16 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
+
 #    browser.get(link)
 #    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
 #    login_link.click()
