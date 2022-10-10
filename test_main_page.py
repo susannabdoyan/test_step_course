35444871c9ca5aca50844febf44d3a922d820ff9
susannabdoyan <bdoyansus@gmail.com>
2022-10-10 14:41:03 +0400

    files added

--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,32 @@
 #import pytest
 #from selenium import webdriver
 #from selenium.webdriver.common.by import By
-from .page_object.locators import MainPageLocators
+import time
 from .page_object.main_page import MainPage
+from .page_object.product_page import ProductPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+#def test_guest_can_go_to_login_page(browser):
+#    link = "http://selenium1py.pythonanywhere.com"
+#    page = MainPage(browser, link)
+#    page.open()
+#    login_page = page.go_to_login_page()
+#    login_page.should_be_login_page()
+#
+#def test_guest_should_see_login_link(browser):
+#    link = "http://selenium1py.pythonanywhere.com/"
+#    page = MainPage(browser, link)
+#    page.open()
+#    login_page = page.should_see_login_link()
+#    login_page.should_see_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    login_page = page.should_see_login_link()
-    login_page.should_see_login_link()
-
-
+    page.go_to_basket()
+    page.element_in_basket()
+    page.basket_is_empty()
+#    time.sleep(50)
 
 
 #    browser.get(link)
